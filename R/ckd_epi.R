b7bcc1740db222dbd52747f12c2d4360db55c67c
coolnamz <coolnamz@naver.com>
2022-04-05 06:44:31 +0000

    fix ckd_epi.R

--- R/ckd_epi.R
+++ R/ckd_epi.R
@@ -3,22 +3,23 @@
 #' Calculate CKD-EPI eGFR
 #'
 #' @param Cr serum creatine value
-#' @param sex sex 
-#' @param age age 
-#' @param text_male word refered to male
-#' @param text_female word refered to female
+#' @param sex sex
+#' @param age age
+#' @param factor_male word refered to male
+#' @param factor_female word refered to female
 #' @return integer value
 #' @keywords ckd_epi
 #' @export
 #'
-ckd_epi = function(Cr, sex, age, text_male="M", text_female="F") {
-  if (!sex %in% c(text_male, text_female)) {
-    stop(paste("Sex variable must be within", text_male, "or", text_female))
-  }
-  
-  GFR = 141 * 
-    pmin(Cr/ifelse(sex==text_female, 0.7, 0.9), 1)^ifelse(sex==text_female, -0.329, -0.411) * 
-    pmax(Cr/ifelse(sex==text_female, 0.7, 0.9), 1)^(-1.209) * 0.993^floor(as.numeric(age)) * 
-    ifelse(sex==text_female, 1.018, 1)
+ckd_epi = function (Cr, sex, age, factor_male, factor_female) {
+
+  score_sex1 = ifelse(sex==factor_female, 0.7, ifelse(sex==factor_male, 0.9, NA))
+  score_sex2 = ifelse(sex==factor_female, -0.329, ifelse(sex==factor_male, -0.411, NA))
+  score_sex3 = ifelse(sex==factor_female, 1.018, ifelse(sex==factor_male, 1, NA))
+
+  GFR = 141 *
+    pmin(Cr/score_sex1, 1)^score_sex2 *
+    pmax(Cr/score_sex1, 1)^(-1.209) * 0.993^floor(as.numeric(age)) *
+    score_sex3
   return(GFR)
 }
