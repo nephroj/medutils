cffdd2f376c7aaebcc46ff816271e031d7168605
coolnamz <coolnamz@naver.com>
2022-04-05 05:48:08 +0000

    Add read_csvk.R

---
+++ R/read_csvk.R
@@ -0,0 +1,13 @@
+#' Read CSV file with EUC-KR encoding
+#'
+#' Read CSV file with EUC-KR encoding
+#'
+#' @param file CSV file
+#' @return data.frame object
+#' @keywords read_csvk
+#' @export
+#'
+read_csvk = function(file, ...) {
+  file_with_encoding = readr::read_csv(file, locale=locale('ko', encoding='euc-kr'), ...)
+  return(file_with_encoding)
+}
