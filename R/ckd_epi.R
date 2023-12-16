ff524c7132578ca1245fa5c79ecf308524eceab8
nephroj <coolnamz@naver.com>
2023-12-16 06:03:13 +0000

    ckd_epi 2021

--- R/ckd_epi.R
+++ R/ckd_epi.R
@@ -11,15 +11,64 @@
 #' @keywords ckd_epi
 #' @export
 #'
-ckd_epi = function (Cr, sex, age, factor_male, factor_female) {
+ckd_epi = function (Cr, sex, age, factor_male, factor_female, black = F, version = 2021) {
 
-  score_sex1 = ifelse(sex==factor_female, 0.7, ifelse(sex==factor_male, 0.9, NA))
-  score_sex2 = ifelse(sex==factor_female, -0.329, ifelse(sex==factor_male, -0.411, NA))
-  score_sex3 = ifelse(sex==factor_female, 1.018, ifelse(sex==factor_male, 1, NA))
+  if (version == 2021) {
 
-  GFR = 141 *
-    pmin(Cr/score_sex1, 1)^score_sex2 *
-    pmax(Cr/score_sex1, 1)^(-1.209) * 0.993^floor(as.numeric(age)) *
-    score_sex3
+    # 2021 version
+    score_sex1 = case_when(
+      sex==factor_female ~ 0.7,
+      sex==factor_male ~ 0.9,
+      TRUE ~ NA_real_
+    )
+    score_sex2 = case_when(
+      sex==factor_female ~ -0.241,
+      sex==factor_male ~ -0.302,
+      TRUE ~ NA_real_
+    )
+    score_sex3 = case_when(
+      sex==factor_female ~ 1.012,
+      sex==factor_male ~ 1,
+      TRUE ~ NA_real_
+    )
+
+    GFR = 142 *
+      pmin(Cr/score_sex1, 1)^score_sex2 *
+      pmax(Cr/score_sex1, 1)^(-1.200) * 0.9938^floor(as.numeric(age)) *
+      score_sex3
+
+  } else if (version == 2009) {
+    # 2009 version
+    score_sex1 = case_when(
+      sex==factor_female ~ 0.7,
+      sex==factor_male ~ 0.9,
+      TRUE ~ NA_real_
+    )
+    score_sex2 = case_when(
+      sex==factor_female ~ -0.329,
+      sex==factor_male ~ -0.411,
+      TRUE ~ NA_real_
+    )
+    score_sex3 = case_when(
+      sex==factor_female ~ 1.018,
+      sex==factor_male ~ 1,
+      TRUE ~ NA_real_
+    )
+    score_black = case_when(
+      black==T ~ 1.159,
+      black==F ~ 1,
+      TRUE ~ NA_real_
+    )
+
+    GFR = 141 *
+      pmin(Cr/score_sex1, 1)^score_sex2 *
+      pmax(Cr/score_sex1, 1)^(-1.209) * 0.993^floor(as.numeric(age)) *
+      score_sex3 * score_black
+
+  } else {
+    GFR = NA_real_
+    print("Select a correct vesion.")
+  }
+
   return(GFR)
 }
